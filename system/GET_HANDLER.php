345eca49bc59477bba78ddcf82741ca86c54b34a
PavelKriko <106550633+PavelKriko@users.noreply.github.com>
2023-01-11 09:31:05 +0600

    Update GET_HANDLER.php

--- system/GET_HANDLER.php
+++ system/GET_HANDLER.php
@@ -30,21 +30,32 @@ class GET_HANDLER{
         if(isset($_GET['Manufacturer']) && !empty($_GET['Manufacturer']) ){
             $query = $pdo->query('SELECT * FROM Manufacturer WHERE ManufacturerName = ' .'"'.$_GET['Manufacturer'].'"' )->fetchAll(PDO::FETCH_ASSOC);
             
-            if (isset($_GET['Model']) && !empty($_GET['Model']) && count($_GET)==2){
-                require './pages/Model.php';
+                if(!empty($query)){
+                
+                if (isset($_GET['Model']) && !empty($_GET['Model']) && count($_GET)==2){
+                    $query_model = $pdo->query('SELECT * FROM Car WHERE Name = ' .'"'.$_GET['Model'].'"' )->fetchAll(PDO::FETCH_ASSOC);
+                    if(!empty($query_model)){
+                        require './pages/Model.php';
+                    }
+                    else{
+                        require './404.php';
+                    }   
+                }
+                if (count($_GET)==1){
+                    require './pages/Manufacter.php';
+                }
+                if(count($_GET)>=3){
+                    require './404.php';
+                }
             }
-            if (count($_GET)==1){
-                require './pages/Manufacter.php';
-            }
-            if(count($_GET)>=3){
+            else{
                 require './404.php';
-            }  
+            }
         }
         if(!array_key_exists('Manufacturer',$_GET)&&!array_key_exists('Manufacturer',$_GET) && count($_GET)>=1){
             require './404.php';
         }
         }
 }
-
 
 ?>
